caf3b85a77ac5ca2d0b09c60665d8ba99f2b9ccb
Brian Boyer <bboyer@npr.org>
2012-09-11 14:55:40 -0400

    integrated states

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
+$(function(){
     
-    var red = Math.floor(Math.random()*538) - 1;
-    var blue = 537 - red;
-    var height = Math.ceil(Math.max(red,blue)/10);
+    // var red = Math.floor(Math.random()*538) - 1;
+    // var blue = 537 - red;
+    // var height = Math.ceil(Math.max(red,blue)/10);
     
-    _.times(red,function(){
-        $("#results .bucket.red").append("<i>V</i>\n");
+    var ds = new Miso.Dataset({ url : "states.csv", delimiter: ',' });
+
+    ds.fetch({
+        success: function() {
+            var red = 0;
+            var blue = 0;
+            this.each(function(row) {
+                //$("#states").append('<li id="state-' + row.abbr + '">' + row.name + ": " + row.winner + "</li>");
+                marker = "<i><span>" + row.stateface + "</span></i>\n"
+                if(row.likely === 'r'){
+                    _.times(row.votes, function(){$("#results .bucket.red").append(marker)});
+                    red = red + row.votes;
+                } else if(row.likely ==='d') {
+                    _.times(row.votes, function(){$("#results .bucket.blue").append(marker)});
+                    blue = blue + row.votes;
+                } else {
+                    _.times(row.votes, function(){$("#results #undecided").append(marker)});
+                }
+            });
+            var height = Math.max(27, Math.ceil(Math.max(red,blue)/10));
+            $("#buckets,.bucket").css("height", height + "em")
+        }
     });
-    _.times(blue,function(){
-        $("#results .bucket.blue").append("<i>V</i>\n");
-    });
-    
-    $("#buckets,.bucket").css("height", height + "em")
 });
