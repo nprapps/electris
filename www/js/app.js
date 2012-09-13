05623fa102f0657b99125fa79fdd81ea5e8ccfce
Christopher Groskopf <staringmonkey@gmail.com>
2012-09-13 11:32:30 -0500

    Track state data in arrays.

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,51 @@
 $(function(){
     var STATE_TEMPLATE = $("#state").html();
+    var red_states = [];
+    var blue_states = [];
+    var undecided_states = [];
     
-    function add_state(row) {
-        var state = _.template(STATE_TEMPLATE, { state: row });
+    function add_state(state) {
+        var html = _.template(STATE_TEMPLATE, { state: state });
 
-        if (row.likely === "r"){
-            $("#results .bucket.red").append(state);
-        } else if(row.likely === "d") {
-            $("#results .bucket.blue").append(state);
+        if (state.likely === "r"){
+            red_states.push(state);
+            $("#results .bucket.red").append(html);
+        } else if(state.likely === "d") {
+            blue_states.push(state);
+            $("#results .bucket.blue").append(html);
         } else {
-            $("#results #undecided").append(state);
+            undecided_states.push(state);
+            $("#results #undecided").append(html);
         }
 
-        $(".state." + row.id + " i").popover({ trigger: "manual" }).click(function(){
+        $(".state." + state.id + " i").popover({ trigger: "manual" }).click(function(){
             $(".state i").popover("hide");
             $(this).popover("show");
         });
     }
 
-    function remove_state(row) {
-        $("div.state." + row.id).remove();
+    function remove_state_from_array(states, state) {
+        var index = 0;
+
+        _.each(states, function(arr_state, i) {
+            if (arr_state.id === state.id) {
+                index = i;
+            }
+        });
+
+        states.splice(index, 1);
+    }
+
+    function remove_state(state) {
+        if (state.likely === "r") {
+            remove_state_from_array(red_states, state);
+        } else if (state.likely === "d") {
+            remove_state_from_array(blue_states, state);
+        } else {
+            remove_state_from_array(undecided_states, state);
+        }
+
+        $("div.state." + state.id).remove();
     }
 
     // var red = Math.floor(Math.random()*538) - 1;
@@ -37,23 +63,17 @@ $(function(){
     ds.fetch();
 
     ds.bind("add", function(event) {
-        var red = 0;
-        var blue = 0;
-
         _.each(event.deltas, function(delta) {
-            var row = delta.changed;
-
-            add_state(row);
-
-            if(row.likely === "r"){
-                red += row.votes;
-            } else if(row.likely ==="d") {
-                blue += row.votes;
-            }
+            add_state(delta.changed);
         });
 
-        var height = Math.max(27, Math.ceil(Math.max(red, blue) / 10));
-        $("#buckets,.bucket").css("height", height + "em");
+        // Sum votes
+        var red_votes = _.reduce(red_states, function(memo, state) { return memo + state.votes; }, 0);
+        var blue_votes = _.reduce(blue_states, function(memo, state) { return memo + state.votes; }, 0);
+        var undecided_votes = _.reduce(undecided_states, function(memo, state) { return memo + state.votes; }, 0);
+
+        //var height = Math.max(27, Math.ceil(Math.max(red_votes, blue_votes) / 10));
+        $("#buckets,.bucket").css("height", 27 + "em");
     });
 
     ds.bind("change", function(event) {
