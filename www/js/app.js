fde046563f8c5c77742ad22fcedab1cf90f72579
Alyson Hurt <ahurt@npr.org>
2012-09-27 01:32:51 -0400

    Responsive tweaks to topline results

--- www/js/app.js
+++ www/js/app.js
@@ -531,12 +531,4 @@ $(function() {
     /* TODO: only the picks that are based on predictions should be reset.
     	picks based on actual live results should not move. */
     	
-    /* TOP TABS */
-    $('#offices').find('li').click(function() {
-    	var tabName = $(this).attr('id').substring(2);
-    	$('#' + tabName).show().siblings('div.row').hide();
-    	$(this).addClass('active').siblings('li').removeClass('active');
-    });
-    $('#o-president').trigger('click');
-    
 });
