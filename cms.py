b8f40527b47288f6a4088e1cdb46af6839708f1b
Jeremy Bowers <jbowers@FNORD-II.local>
2012-10-04 16:36:18 -0400

    First round of updates to the big board app. Creates www.py. Updates and adds (non working) functions to cms.py for house/senate admin.

--- cms.py
+++ cms.py
@@ -2,24 +2,81 @@
 
 from datetime import datetime
 import json
+from sets import Set
 
 from flask import Flask
 from flask import render_template, request
 import pytz
 
 import cms_settings as settings
-from util import get_database, get_states, regenerate_csv, push_results_to_s3
+from util import (
+    get_database, get_states, regenerate_csv, push_results_to_s3, get_house_candidates, get_senate_candidates)
+from fetch_ap_results import CANDIDATE_MODEL_FIELDS
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def winners():
+@app.route('/', methods=['GET'])
+def race_list():
     """
-    Read/update list of state winners.
+    Return a list of races in case someone gets to the main page.
+    """
+
+    return """
+        <html>
+            <body>
+                <h1>Admin: Races</h1>
+                <a href='/president/'>Presidential</a><br/>
+                <a href='/house/'>U.S. House</a><br/>
+                <a href='/senate'>U.S. Senate</a>
+            </body>
+        </html>
+    """
+
+@app.route('/house/', methods=['GET'])
+def house_candidates():
+    """
+    Read/update list of results-by-district for the U.S. House election.
+    This is limited to the "state_candidates" table in the SQLite DB.
+    """
+    db = get_database()
+    candidates = get_house_candidates(db)
+    db.close()
+
+    context = {
+        'settings': settings,
+        'candidates': candidates,
+        'race': 'U.S. House'
+    }
+
+    return render_template('house_senate_cms.html', **context)
+
+@app.route('/senate/', methods=['GET'])
+def senate_candidates():
+    """
+    Read/update list of results-by-state for the U.S. Senate election.
+    This is limited to the "state_candidates" table in the SQLite DB.
+    """
+    db = get_database()
+    candidates = get_senate_candidates(db)
+    db.close()
+
+    context = {
+        'settings': settings,
+        'candidates': candidates,
+        'race': 'U.S. Senate'
+    }
+
+    return render_template('house_senate_cms.html', **context)
+
+@app.route('/president/', methods=['GET', 'POST'])
+def presidential_results():
+    """
+    Read/update list of results-by-state for the Presidential election.
+    This is limited to the "states" table in the SQLite DB.
     """
     db = get_database()
     states = get_states(db)
-        
+
     if request.method == 'POST':
         for state in states:
             prediction = request.form.get('prediction-%s' % state['id'], '')
@@ -46,12 +103,13 @@ def winners():
 
     context = {
         'settings': settings,
-        'states': states
+        'states': states,
+        'race': 'Presidential'
     }
 
-    return render_template('winners.html', **context)
+    return render_template('president_cms.html', **context)
 
-@app.route('/data', methods=['GET', 'POST'])
+@app.route('/president/data/', methods=['GET', 'POST'])
 def data():
     db = get_database()
     states = get_states(db)
