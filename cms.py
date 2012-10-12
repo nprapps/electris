d19896abac792eab15809e15ca4ef2895e0cd50a
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-10-12 15:50:22 -0400

    Updates CMS to add routes for house and senate. Add house_senate.html template.

--- cms.py
+++ cms.py
@@ -6,16 +6,42 @@ import json
 from flask import Flask
 from flask import render_template, request
 import pytz
+from operator import itemgetter
 
 import cms_settings as settings
-from util import get_database, get_states, regenerate_president, push_results_to_s3
+from util import get_database, get_states, regenerate_president, push_results_to_s3, get_house_senate
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def winners():
+
+@app.route('/races/<house>/', methods=['GET', 'POST'])
+def house(house):
     """
-    Read/update list of state winners.
+    Read/update list of house candidates.
+    """
+
+    house_slug = 'H'
+    if house == 'senate':
+        house_slug = 'S'
+
+    query = get_house_senate(get_database())
+    candidates = []
+    for candidate in query:
+        if candidate['office_code'] == house_slug.upper():
+            if candidate['party'] in ['Dem', 'GOP']:
+                candidates.append(candidate)
+    context = {
+        'settings': settings,
+        'candidates': sorted(candidates, key=itemgetter(21, 0, 3)),
+        'house': house
+    }
+    return render_template('house_senate.html', **context)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def president():
+    """
+    Read/update list of presidential state winners.
     """
     db = get_database()
     states = get_states(db)
@@ -60,4 +86,3 @@ def data():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=settings.DEBUG)
-
