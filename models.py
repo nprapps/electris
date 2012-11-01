b25fb6cd9809b10828880039ed5826064ffa1517
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-11-01 17:06:01 -0400

    God. Dammit.

--- models.py
+++ models.py
@@ -272,7 +272,7 @@ class State(Model):
 
     def rep_vote_percent(self):
         if self.rep_vote_count > 0:
-            getcontext().prec = 2
+            getcontext().prec = 3
             percent = Decimal(self.rep_vote_count) / Decimal(self.dem_vote_count + self.rep_vote_count)
             return round(float(percent) * 100, 0)
         else:
@@ -280,7 +280,7 @@ class State(Model):
 
     def dem_vote_percent(self):
         if self.dem_vote_count > 0:
-            getcontext().prec = 2
+            getcontext().prec = 3
             percent = Decimal(self.dem_vote_count) / Decimal(self.dem_vote_count + self.rep_vote_count)
             return round(float(percent) * 100, 0)
         else:
