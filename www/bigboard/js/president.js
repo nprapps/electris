06b3bd85ec9763033513bbd411a1efaabfddc273
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-11-04 09:42:08 -0500

    Adds president fix for boards.

--- www/bigboard/js/president.js
+++ www/bigboard/js/president.js
@@ -8,7 +8,7 @@ $(function(){
 
             _.each(timezones, function(timezone){
                 var html = TIMEZONE_TEMPLATE({ timezone: timezone });
-                $('#candidates .full').append(html);
+                $('#candidates .initial').append(html);
                 _.each(timezone.states, function(state){
                     if (state.prediction == 't'){ featured_array.push(state); }
                 });
@@ -19,8 +19,16 @@ $(function(){
                 $('#candidates .weighted').append(html);
             });
 
+            _.each($('.state_row'), function(row, index, list){
+                var page = 1;
+                if (index + 1 > 22 ) { page = 2; }
+                if (index + 1 > 44) { page = 3; }
+                $('#page-'+ page).append(row);
+            });
+
+            // $('#candidates .full').columnize({ columns:3 });
             $('#candidates .weighted').columnize({ columns:2 });
-            $('#candidates .full').columnize({ columns:3 });
+
         });
     }
 
@@ -35,13 +43,13 @@ $(function(){
     fetchBOP();
     fetchData();
 
-    var polling_interval = 15;
+    var polling_interval = 10;
     var countdown = polling_interval;
-   function refresh_countdown() {
+    function refresh_countdown() {
         countdown -= 1;
         if (countdown === 0) {
-            nukeTarget('#candidates .full');
             nukeTarget('#candidates .weighted');
+            nukeTarget('#page-1, #page-2, #page-3');
             fetchBOP();
             fetchData();
             countdown = polling_interval + 1;
