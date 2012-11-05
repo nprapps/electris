e3bd0b06368d453f15cf9d75f1f8cf72f28f0552
Christopher Groskopf <staringmonkey@gmail.com>
2012-11-04 19:04:19 -0500

    PSQL initial data.

--- fabfile.py
+++ fabfile.py
@@ -303,14 +303,12 @@ def update_fake_ap_data():
     """
     Gets randomly assigned data from snapshot files in our timemachine.
     """
-    local('echo "`date` starting update_fake_ap_data" >> fake_ap_data.log')
     data = i.get_fake_ap_data()
     ne_data = i.get_fake_ap_district_data('NE')
     me_data = i.get_fake_ap_district_data('ME')
 
     i.parse_ap_data(data, ne_data, me_data)
     write_www_files()
-    local('echo "`date` finishing update_fake_ap_data" >> fake_ap_data.log')
 
 
 def build():
