06b6376d86b007e598018b890fa1f4cb1ee24710
Christopher Groskopf <staringmonkey@gmail.com>
2012-11-05 10:53:03 -0500

    Remove debug statements.

--- www/js/app.js
+++ www/js/app.js
@@ -1003,8 +1003,6 @@ $(function() {
         var changes = false;
         var states_length = states.length;
 
-        console.log(states_length);
-
         for (var i = 0; i < states_length; i++) {
             var old_state = states[i];
             var state = data[i];
@@ -1014,10 +1012,6 @@ $(function() {
                 old_state["rep_vote_count"] != state["rep_vote_count"] ||
                 old_state["precincts_reporting"] != state["precincts_reporting"]) {
 
-                console.log(state.name);
-                console.log('old: ' + old_state["call"]);
-                console.log('new: ' + state["call"]);
- 
                 $(".state." + state.id).remove();
                 add_state(state, true);
 
